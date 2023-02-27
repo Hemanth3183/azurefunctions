8a3473a0e6a6684a5f4bf4557306f11f76917fef
Hemi <hemant.nalige@gmail.com>
2023-02-27 18:30:11 +0800

    name to firstname

--- HttpExample.cs
+++ HttpExample.cs
@@ -19,15 +19,15 @@ namespace FunctionApp1
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string firstname = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            firstname = firstname ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            string responseMessage = string.IsNullOrEmpty(firstname)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {firstname}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
