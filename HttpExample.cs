70bd14f69b865b0c82f960ed99381dee45236b2e
Hemi <hemant.nalige@gmail.com>
2023-02-27 20:04:16 +0800

    4

--- HttpExample.cs
+++ HttpExample.cs
@@ -27,7 +27,7 @@ namespace FunctionApp1
 
             string responseMessage = string.IsNullOrEmpty(firstname)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {firstname}. What the hell is going on???";
+                : $"Hello, {firstname}. Come on please work!!";
 
             return new OkObjectResult(responseMessage);
         }
